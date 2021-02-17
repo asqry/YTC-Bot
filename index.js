80722e360a5cdb600eda32aecfb9ae77283c784c
The0Show <marrerodad2@gmail.com>
2021-02-17 16:15:23 -0500

    did stuff
    • Added node-fetch
    • Made the discord.js const just "Discord", to mention all discord.js functions without needing to mention it in the const
    • Added bot status

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { Client, MessageEmbed } = require('discord.js');
-const client = new Client({
+const Discord = require('discord.js');
+const client = new Discord.Client({
   partials: ['CHANNEL', 'GUILD_MEMBER', 'MESSAGE', 'REACTION', 'USER'],
 });
+const fetch = require('node-fetch')
 
 const mongoose = require('mongoose');
 mongoose.connect(
@@ -29,6 +30,7 @@ client.on('ready', () => {
   console.log('Updated filter!');
 
   console.log('Bot is up and running!');
+  client.user.setActivity({name: 'you', type: 'WATCHING'})
 });
 
 client.on('message', async (message) => {
@@ -47,10 +49,8 @@ client.on('message', async (message) => {
   }
   if (command === 'report') {
     runReportCmd(message, args);
-  } else if (
-    command === 'clearstory' &&
-    message.member.permissions.has('ADMINISTRATOR')
-  ) {
+  }
+  if (command === 'clearstory' && message.member.permissions.has('ADMINISTRATOR')) {
     message.delete();
     const storyObj = await storyModel.findOne({});
     storyObj.story = '';
@@ -58,19 +58,55 @@ client.on('message', async (message) => {
     message
       .reply('Successfully reset the story.')
       .then((m) => m.delete({ timeout: 5000 }));
-  } else if (
-    command === 'currentstory' &&
-    message.member.permissions.has('MANAGE_MESSAGES')
-  ) {
+  }
+  if (command === 'currentstory' && message.member.permissions.has('MANAGE_MESSAGES')) {
     message.delete();
     const storyObj = await storyModel.findOne({});
 
     message.channel.send(
-      new MessageEmbed()
+      new Discord.MessageEmbed()
         .setTitle('Current Story!')
         .setDescription(storyObj.story.replace(/\\n/gim, '\n'))
         .setTimestamp()
     );
+  }
+  if(command === 'status'){
+    const validStatuses = ['smp']
+    if(!args.length) return
+    if(!validStatuses.includes(args[0])) return
+    if(args[0] === 'smp'){
+      const lektonicdata = await fetch('https://api.mcsrvstat.us/2/ytc.lektonic.com').then(response => response.json());
+      const worlddata = await fetch('https://api.mcsrvstat.us/2/play.ytc.world').then(response => response.json());
+      let lektonic
+      let world
+      let lektonicmotd
+      let worldmotd
+
+      if(!lektonicdata.online){
+        lektonic = '❌ Offline or Unreachable'
+      } else {
+        if(lektonicdata.motd.clean[0]) lektonicmotd = lektonicdata.motd.clean[0]
+        if(lektonicdata.motd.clean[1]) lektonicmotd = lektonicmotd + `\n` + lektonicdata.motd.clean[1]
+        lektonic = `✅ Online\n\nVersion: ${lektonicdata.version}\nMOTD: \`\`\`\n${lektonicmotd}\n\`\`\`\nPlayers: ${lektonicdata.players.online}/${lektonicdata.players.max}\nSoftware: ${lektonicdata.software}`
+      }
+
+      if(!worlddata.online){
+        world = '❌ Offline or Unreachable'
+      } else {
+        if(worlddata.motd.clean[0]) worldmotd = worlddata.motd.clean[0]
+        if(worlddata.motd.clean[1]) worldmotd = worldmotd + `\n` + worlddata.motd.clean[1]
+        world = `✅ Online\n\nVersion: ${worlddata.version}\nMOTD: \n\`\`\`\n${worldmotd}\n\`\`\`\nPlayers: ${worlddata.players.online}/${worlddata.players.max}\nSoftware: ${worlddata.software}`
+      }
+
+      const embed = new Discord.MessageEmbed()
+      .setTitle('YTC SMP Status')
+      .addField('ytc.lektonic.com', lektonic)
+      .addField('play.ytc.world', world)
+      .setTimestamp()
+
+      message.channel.send(embed)
+
+    }
   }
 });
 
